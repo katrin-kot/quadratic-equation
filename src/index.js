f8e4c20717de04a33538b636d5e45d09ddead21d
katrin-kot <enkor89@mail.ru>
2018-09-15 21:59:48 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+const arr = equation.split (' ');
+const a=arr[0];
+const b=arr[3]+arr[4];
+const c=arr[7]+arr[8];
+const d=b*b-4*a*c;
+if (d>=0){
+  const x1=Math.round((-b-Math.sqrt(d))/(2*a));
+  const x2=Math.round((-b+Math.sqrt(d))/(2*a));
+  if (x1>x2){
+  return [x2,x1];}
+  else {return [x1,x2];}
+  }
 }
